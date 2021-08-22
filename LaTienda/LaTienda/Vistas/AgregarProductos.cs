1f58afaaaa97f042c86bb9d84cad3586e8d8ca93
dantegambarte <da.nt3@hotmail.com>
2021-08-21 21:46:04 -0300

    Iteracion 3

--- LaTienda/LaTienda/Vistas/AgregarProductos.cs
+++ LaTienda/LaTienda/Vistas/AgregarProductos.cs
@@ -16,5 +16,10 @@ namespace LaTienda.Vistas
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
